5e7687aa1a1dda41c12db9abb36c10319e19555b
Paul Peyrard <paul.peyrard17@gmail.com>
2023-03-10 12:43:07 +0100

    ajout segmentation par département

--- segmentation_paul.py
+++ segmentation_paul.py
@@ -2,8 +2,8 @@ with open("../fichier_sans_lignes_vides.txt", "r", encoding="utf8") as f:
     lines = f.readlines()
 
 lines = [line.strip() for line in lines if line.strip()]
-d_title = {}
 
+d_title = {}
 for i in range(len(lines)):
     if lines[i][0:5] == "<text":
         p = ""
@@ -19,9 +19,8 @@ for i in range(len(lines)):
             d_title[p].append(i)
         else:
             d_title[p] = [i]
-    # if i > 50:
-    #     break
-    
+    if i > 50:
+        break 
 # print(d_title)
 
 for elt in d_title:
@@ -29,3 +28,27 @@ for elt in d_title:
         print(elt,"---",len(d_title[elt]))  
 #affiche le nombre de thèmes qui apparaisse + de 2 fois, et c'est souvent 2 fois max donc pas fou de faire une analyse sur les thèmes sauf si on les généralise mais c'est risqué sur des questions précises
 
+
+d_dept = {}
+for i in range(len(lines)):
+    if lines[i][0:5] == "<text":
+        p = ""
+        boo = False
+        for j in range(len(lines[i])):
+            if lines[i][j-12:j] == 'department="':
+                boo = True
+            if lines[i][j] == '"':
+                boo = False
+            if boo != False:
+                p += lines[i][j]
+        if p in d_dept:  #ajoute dans un dictionnaire le thème et son itération correspondante dans une liste
+            d_dept[p].append(i)
+        else:
+            d_dept[p] = [i]
+    # if i > 50:
+    #     break
+# print(d_dept)
+
+for elt in d_dept:
+    print(elt,"---",len(d_dept[elt])) 
+#par départment c'est déjà mieux (données sur 100 départements)
