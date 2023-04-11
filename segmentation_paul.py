3b40dffc6fc0c143f4e34df57108d61bb8e0c82c
Paul Peyrard <paul.peyrard17@gmail.com>
2023-04-11 15:40:05 +0200

    fin liste  dictionnaire en fct des deps avec dic questions, dic reponses

--- segmentation_paul.py
+++ segmentation_paul.py
@@ -31,7 +31,29 @@ for i in range(len(lines)):
 #affiche le nombre de thèmes qui apparaisse + de 2 fois, et c'est souvent 2 fois max donc pas fou de faire une analyse sur les thèmes sauf si on les généralise mais c'est risqué sur des questions précises
 
 
-d_dept = {}
+# d_dept_quest = {}
+# for i in range(len(lines)):
+#     if lines[i][0:5] == "<text":
+#         p = ""
+#         boo = False
+#         for j in range(len(lines[i])):
+#             # if lines[i][j-12:j] == 'department="': #par nom de departement
+#             # if lines[i][j-8:j] == 'region="':  #par region
+#             if lines[i][j-15:j] == 'departmentiso="':  #par numéro de departement
+#                 boo = True
+#             if lines[i][j] == '"':
+#                 boo = False
+#             if boo != False:
+#                 p += lines[i][j]
+#         if p in d_dept_quest:  #ajoute dans un dictionnaire le thème et son itération correspondante dans une liste
+#             # print(lines[i])
+#             d_dept_quest[p].append(lines[i])
+#         else:
+#             d_dept_quest[p] = [lines[i]]
+    # if i > 100:
+    #     break
+
+d_dept_rep = {}
 for i in range(len(lines)):
     if lines[i][0:5] == "<text":
         p = ""
@@ -45,19 +67,27 @@ for i in range(len(lines)):
                 boo = False
             if boo != False:
                 p += lines[i][j]
-        if p in d_dept:  #ajoute dans un dictionnaire le thème et son itération correspondante dans une liste
-            d_dept[p].append(i)
-        else:
-            d_dept[p] = [i]
-    # if i > 100000:
-    #     break
-# print(d_dept)
+    else:
+        if lines[i] != "</text>":
+            if p in d_dept_rep:  #ajoute dans un dictionnaire le thème et son itération correspondante dans une liste
+                # print(lines[i])
+                d_dept_rep[p].append(lines[i])
+            else:
+                d_dept_rep[p] = [lines[i]]
+    if i > 100:
+        break
 
-for elt in d_dept:
-    d_dept[elt] = len(d_dept[elt])
-    print(elt,"---",d_dept[elt]) 
+# for elt in d_dept_quest:
+#     d_dept_quest[elt] = len(d_dept_quest[elt])
+#     print(elt,"---",d_dept_quest[elt]) 
+# print("\n\n")
+
+# for elt in d_dept_rep:
+#     d_dept_rep[elt] = len(d_dept_rep[elt])
+#     print(elt,"---",d_dept_rep[elt]) 
 #par départment c'est déjà mieux (données sur 100 départements)
 
 #trier de manière décroissante
-sorted_d = dict(sorted(d_dept.items(), key=lambda x: x[1], reverse=True))
+sorted_d = dict(sorted(d_dept_rep.items(), key=lambda x: len(x[1]), reverse=True))
 print(sorted_d)
+
